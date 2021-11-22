48e3c2e218b47caa2bc932057efa4e9dfe44f76f
jacobbbyy <aliefalieman@gmail.com>
2021-11-22 16:04:55 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Accordion from './components/Accordion';
+
+function App() {
+  return(
+    <>
+     <div className='faq--container'><h1>FAQ</h1></div>
+     
+     <Accordion />;
+     </>
+  )
+
+  
+}
+
+export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,107 @@
+import React, { useState } from 'react';
+import { Data } from './Data';
+import styled from 'styled-components';
+import { IconContext } from 'react-icons';
+import { FiPlus, FiMinus } from 'react-icons/fi';
+
+const AccordionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  height: 100vh;
+  background: #fff;
+  
+`;
+
+
+const Container = styled.div`
+  position: absolute;
+  top: 1%;
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
+
+const Wrap = styled.div`
+  background: #272727;
+  color: #F8F5B3;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
+
+  h1 {
+    margin-right: 500px;
+    padding: 2rem;
+    font-size: 2rem;
+  }
+
+  span {
+    margin-right: 1.5rem;
+  }
+`;
+
+const Dropdown = styled.div`
+  background: #1c1c1c;
+  color: #F8F5B3;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: left;
+  padding-left: 30px;
+ 
+
+  p {
+    font-size: 2rem;
+  }
+`;
+
+const Accordion = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = index => {
+    if (clicked === index) {
+      //if clicked question is already active, then close it
+      return setClicked(null);
+    }
+
+    setClicked(index);
+  };
+
+  return (
+    <IconContext.Provider value={{ color: '#F8F5B3', size: '25px' }}>
+     
+
+
+      <AccordionSection>  
+        
+        <Container>
+          {Data.map((item, index) => {
+            return ( 
+              <>
+       
+        
+                <Wrap onClick={() => toggle(index)} key={index}>
+                  <h1>{item.question}</h1>
+                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+                </Wrap>
+                {clicked === index ? (
+                  <Dropdown>
+                    <p>{item.answer}</p>
+                  </Dropdown>
+                ) : null}
+              
+              </>
+            );
+          })}
+        </Container>
+      </AccordionSection>
+    </IconContext.Provider>
+  );
+};
+
+export default Accordion;
